17e2114cc027639ec08a2b821840e2356e2fd570
Darwin-our-lord <David@prebensen.dk>
2026-04-30 11:06:19 +0200

    idk

---
+++ FORTNOTEGAME/Assets/scripts/CatRiding.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CatRiding : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(this.gameObject.transform);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
