83eb0dcde5e32c1fdbad6ef19ca08a741834301b
Darwin-our-lord <David@prebensen.dk>
2026-05-27 11:11:19 +0200

    color change

--- FORTNOTEGAME/Assets/scripts/PressurePlate.cs
+++ FORTNOTEGAME/Assets/scripts/PressurePlate.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
 
 public enum PressurePlateFunction
 {
@@ -13,13 +14,16 @@ public class PressurePlate : MonoBehaviour
 
     [Header("DOOR")]
     [SerializeField] GameObject[] targetDoor;
-
+    ///*[SerializeField]*/ static Material material;
+    
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name);
         if (other.CompareTag("Cat") || other.CompareTag("Player")&&!onlyCat)
         {
             Debug.Log(other.name + "   2");
+            GetComponent<MeshRenderer>().material.color = Color.darkRed;
+
             switch (plateFunction)
             {
                 case PressurePlateFunction.None:
